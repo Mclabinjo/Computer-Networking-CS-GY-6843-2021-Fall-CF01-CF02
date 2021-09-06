90596b4cb55ae33bd3cc403091dd177e4857a3ba
Mclabinjo <90166201+Mclabinjo@users.noreply.github.com>
2021-09-05 23:27:12 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,10 @@
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
 
-
-
-
+def welcome_assignment_answers(question):
+    #The student doesn't have to follow the skeleton for this assignment.
+    #Another way to implement is using a "case" statements similar to C.
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?, you have to hunt for this one in slack":
         answer = "mTLS"
     elif question == "Are encoding and encryption the same? - Yes/No":
@@ -20,6 +23,15 @@
         answer = "2"
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = "3"
+    return (answer)
+# Complete all the questions.
 
-    return(answer)
+
+if __name__ == "__main__":
+    #use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+
+
+
 
