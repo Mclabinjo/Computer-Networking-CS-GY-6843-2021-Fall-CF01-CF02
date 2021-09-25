240f1901c92c277f2e9f929401ea359f8ea96a8a
mayowalabinjo <ol2089@nyu.edu>
2021-09-25 18:31:51 -0500

    hi

--- solution.py
+++ solution.py
@@ -10,8 +10,9 @@ def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
   serverSocket.bind(("", port))
+  serverSocket.listen(1)
   #Fill in start
-
+  print 'the web server is up on port:', port
   #Fill in end
 
   while True:
@@ -23,25 +24,27 @@ def webServer(port=13331):
 
       try:
         message = connectionSocket.recv(1024) #Fill in start    #Fill in end
+        print message, '::', message.split()[0], ':', message.split()[1]
         filename = message.split()[1]
+        print filename, '||', filename[1:]
         f = open(filename[1:])
         outputdata = f.read() #Fill in start     #Fill in end
         print(outputdata)
         #Send one HTTP header line into socket.
         #Fill in start
-        connectionSocket.send('\nHTTP/1.1 200 OK\n\n'.encode())
+        connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
+        connectionSocket.send(outputdata)
         #Fill in end
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
-          connectionSocket.send(outputdata[i].encode())
+          connectionSocket.send(outputdata[i])
 
-        connectionSocket.send("\r\n".encode())
         connectionSocket.close()
       except IOError:
         # Send response message for file not found (404)
         #Fill in start
-        connectionSocket.send("\nHTTP/1.1 404 Not Found\n\n".encode())
+        connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
         #Fill in end
 
 
