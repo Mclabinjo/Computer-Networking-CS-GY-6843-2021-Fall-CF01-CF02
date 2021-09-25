65fea245420a281a38d2f67b7c1eaa1e027519fc
mayowalabinjo <ol2089@nyu.edu>
2021-09-25 18:46:04 -0500

    hi

--- solution.py
+++ solution.py
@@ -55,11 +55,10 @@ def webServer(port=13331):
 
     except (ConnectionResetError, BrokenPipeError):
       serverSocket.close()
-      sys.exit()
       pass
 
 
-    # Terminate the program after sending the corresponding data
+  sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
   webServer(13331)
